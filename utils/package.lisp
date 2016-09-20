2bac916f54a1ae4680f062bef154fdc0c548506f
shamazmazum <shamaz.mazum@gmail.com>
2016-09-20 22:51:44 +0300

    Remove unused package

--- utils/package.lisp
+++ utils/package.lisp
@@ -4,18 +4,3 @@
   (:export #:mixchannels
            #:write-pcm-wav-header
            #:defreader))
-
-(defpackage easy-audio.utils-sbcl
-  (:nicknames #:utils-sbcl)
-  (:use #:cl #:easy-audio-early)
-  (:import-from #:sb-vm
-                #:signed-reg #:signed-num #:descriptor-reg #:any-reg
-                #:simple-array-signed-byte-32
-                #:punpckldq #:movd #:movdqa
-                #:int-sse-reg #:simd-pack-int #:simd-pack
-                #:flushable #:foldable)
-  (:import-from #:sb-c
-                #:defknown
-                #:define-vop
-                #:inst #:move)
-  (:export #:make-simd-pack-sb32 #:store-simd-pack-to-sa-sb32))
