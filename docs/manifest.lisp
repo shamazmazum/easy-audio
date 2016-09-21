55a642f0576343a24e98e68aa9a0fa26b56282e9
shamazmazum <shamaz.mazum@gmail.com>
2016-09-21 12:28:52 +0300

    Codex documentation

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:easy-audio)
+ :documents ((:title "Easy Audio"
+              :authors ("Vasily Postnicov")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))
