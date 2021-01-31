f056f23cd5cf962bb93cd4114174857c694206f9
Vasily Postnicov <shamaz.mazum@gmail.com>
2021-01-31 21:45:34 +0300

    Read seektable and calculate auxiliary metadata fields

--- ape/definitions.lisp
+++ ape/definitions.lisp
@@ -11,12 +11,13 @@
 (define-condition apev2-tag-error (ape-error) ()
   (:documentation "APEv2 tag error"))
 
-(defstruct (metadata-header (:conc-name nil))
+(defstruct metadata
+  (version            0 :type (ub 16))
   (padding1           0 :type (ub 16))
   (desc-len           0 :type (ub 32))
   (header-len         0 :type (ub 32))
   (seektable-len      0 :type (ub 32))
-  (waveheader-len     0 :type (ub 32))
+  (wavheader-len      0 :type (ub 32))
   (audiodata-len      0 :type (ub 32))
   (audiodata-len-high 0 :type (ub 32))
   (wavtail-len        0 :type (ub 32))
@@ -28,7 +29,11 @@
   (total-frames       0 :type (ub 32))
   (bps                0 :type (ub 16))
   (channels           0 :type (ub 16))
-  (samplerate         0 :type (ub 32)))
+  (samplerate         0 :type (ub 32))
+  ;; Derived fields
+  (first-frame        0 :type (ub 32))
+  (total-samples      0 :type (ub 32))
+  seektable)
 
 (defgeneric read-metadata-header (reader ape-version)
   (:documentation "Read and fill METADATA-HEADER structure"))
