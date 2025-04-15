8d67ed4337642a5e6f2d41fffacd6ab3b7d3bdc4
Vasily Postnicov <shamaz.mazum@gmail.com>
2025-04-15 09:02:48 +0300

    Refactor CRC calculation

--- bitreader/crc.lisp
+++ bitreader/crc.lisp
@@ -1,35 +1,33 @@
 (in-package :easy-audio.bitreader)
 
-(declaim (optimize #+easy-audio-unsafe-code
-                   (safety 0) (speed 3)))
-
 ;; Contains CRC functions used by easy-audio
 ;; Flac
 (declaim (type (sa-ub 16) +crc-table-0-8005+))
-(defparameter +crc-table-0-8005+
-  (make-array 256
-              :element-type '(ub 16)
-              :initial-contents
-              '#.(flet ((crc-for-byte (byte)
-                          (declare (type (ub 8) byte))
-                          (let ((crc (ash byte 8)))
-                            (declare (type (ub 16) crc))
-                            (loop repeat 8 do
-                                 (setq crc
-                                       (logand #xffff
-                                               (if (/= 0 (logand #x8000 crc))
-                                                   (logxor #x8005 (ash crc 1))
-                                                   (ash crc 1))))
-                               finally (return crc)))))
-                   (loop for i below 256 collect (crc-for-byte i))))
-  "Precalculated CRC-16 table, starting with 0, polynomial generator #x8005.
-   Used for FLAC")
+(define-constant +crc-table-0-8005+
+    (make-array 256
+                :element-type '(ub 16)
+                :initial-contents
+                '#.(flet ((crc-for-byte (byte)
+                            (labels ((%go (crc n)
+                                       (if (zerop n) crc
+                                           (%go
+                                            (logand (if (zerop (logand #x8000 crc))
+                                                        (ash crc 1)
+                                                        (logxor #x8005 (ash crc 1)))
+                                                    #xffff)
+                                            (1- n)))))
+                              (%go (ash byte 8) 8))))
+                     (loop for i below 256 collect (crc-for-byte i))))
+  :documentation "Precalculated CRC-16 table, starting with 0,
+polynomial generator #x8005.  Used for FLAC."
+  :test #'equalp)
 
-(declaim (ftype (function ((sa-ub 8) (ub 16)
-                           &key (:start t) (:end t)) (ub 16)) crc-0-8005))
+(serapeum:-> crc-0-8005 ((sa-ub 8) (ub 16)
+                         &key (:start t) (:end t))
+             (values (ub 16) &optional))
 (defun crc-0-8005 (array accum &key (start 0) end)
   "CRC checksum used in FLAC frames"
-  (declare (type (sa-ub 8) array))
+  (declare (optimize (speed 3)))
   (flet ((accumulate-crc (crc x)
                          (declare (type (ub 16) crc)
                                   (type (ub 8) x))
@@ -41,31 +39,31 @@
 
 ;; OGG
 (declaim (type (sa-ub 32) +crc-table-0-04c11db7+))
-(defparameter +crc-table-0-04c11db7+
-  (make-array 256
-              :element-type '(ub 32)
-              :initial-contents
-              '#.(flet ((crc-for-byte (byte)
-                          (declare (type (ub 8) byte))
-                          (let ((crc (ash byte 24)))
-                            (declare (type (ub 32) crc))
-                            (loop repeat 8 do
-                                 (setq crc
-                                       (logand #xffffffff
-                                               (if (/= 0 (logand #x80000000 crc))
-                                                   (logxor #x04c11db7 (ash crc 1))
-                                                   (ash crc 1))))
-                                 finally (return crc)))))
+(define-constant +crc-table-0-04c11db7+
+    (make-array 256
+                :element-type '(ub 32)
+                :initial-contents
+                '#.(flet ((crc-for-byte (byte)
+                            (labels ((%go (crc n)
+                                       (if (zerop n) crc
+                                           (%go
+                                            (logand (if (zerop (logand #x80000000 crc))
+                                                        (ash crc 1)
+                                                        (logxor #x04c11db7 (ash crc 1)))
+                                                    #xffffffff)
+                                            (1- n)))))
+                              (%go (ash byte 24) 8))))
+                     (loop for i below 256 collect (crc-for-byte i))))
+  :documentation "Precalculated CRC-32 table, starting with 0,
+polynomial generator #x04c11db7. Used for OGG container."
+  :test #'equalp)
 
-                   (loop for i below 256 collect (crc-for-byte i))))
-  "Precalculated CRC-32 table, starting with 0, polynomial generator #x04c11db7.
-   Used for OGG container")
-
-(declaim (ftype (function ((sa-ub 8) (ub 32)
-                           &key (:start t) (:end t)) (ub 32)) crc-0-04c11db7))
+(serapeum:-> crc-0-04c11db7 ((sa-ub 8) (ub 32)
+                             &key (:start t) (:end t))
+             (values (ub 32) &optional))
 (defun crc-0-04c11db7 (array accum &key (start 0) end)
   "CRC checksum used in OGG container"
-  (declare (type (sa-ub 8) array))
+  (declare (optimize (speed 3)))
   (flet ((accumulate-crc (crc x)
                          (declare (type (ub 32) crc)
                                   (type (ub 8) x))
