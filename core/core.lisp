a31fc98f608304b03e7ec653967718e020916301
Vasily Postnicov <shamaz.mazum@gmail.com>
2025-05-21 15:38:10 +0300

    Optimize MIXCHANNELS-N

--- core/core.lisp
+++ core/core.lisp
@@ -9,13 +9,13 @@
 
 ;; For interactive restarts
 (sera:defvar-unbound *current-condition*
-  "*CURRENT-CONDITION* is bound to signaled contition
-   when debugger is invoked while within WITH-INTERACTIVE-DEBUG")
+  "*CURRENT-CONDITION* is bound to signaled contition when debugger is
+invoked while within WITH-INTERACTIVE-DEBUG.")
 
 (defmacro with-interactive-debug (&body body)
   "If any condition is signaled and the debugger is invoked while
-   within this macro, *CURRENT-CONDITION* will be bound to the
-   condition signaled"
+within this macro, *CURRENT-CONDITION* will be bound to the condition
+signaled."
   (let ((debugger-hook (gensym)))
     `(let ((,debugger-hook *debugger-hook*))
        (flet ((,debugger-hook (condition me)
@@ -28,49 +28,45 @@
            ,@body)))))
 
 ;; Utility functions
+(sera:-> mixchannels-n ((sa-sb 32) list)
+         (values (sa-sb 32) &optional))
 (defun mixchannels-n (out buffers)
-  (declare (type list buffers)
-	   (type (simple-array (signed-byte 32)) out)
-	   (optimize #+easy-audio-unsafe-code
-                     (safety 0) (speed 3)))
-  (let ((offset (length buffers))
-	(size (length (the (simple-array (signed-byte 32))
-			   (nth 0 buffers))))
-	(idx 0))
-    (declare (type fixnum offset size idx))
-    (dotimes (i size)
-      (dotimes (j offset)
-	(declare (type fixnum i j))
-	(setf (aref out (+ idx j))
-	      (aref (the (simple-array (signed-byte 32))
-		         (nth j buffers))
-                    i)))
-      (incf idx offset))
-    out))
+  (declare (optimize (speed 3)))
+  (let* ((channels (length buffers))
+         (first-buffer (first buffers))
+         (samples (length first-buffer)))
+    (declare (type (sa-sb 32) first-buffer))
+    (loop for s fixnum below samples
+          for idx fixnum from 0 by channels do
+          (loop for buffer of-type (sa-sb 32) in buffers
+                for c fixnum from 0 by 1 do
+	        (setf (aref out (+ idx c))
+	              (aref buffer s)))))
+  out)
 
+(sera:-> mixchannels-2 ((sa-sb 32) (sa-sb 32) (sa-sb 32))
+         (values (sa-sb 32) &optional))
 (defun mixchannels-2 (output channel1 channel2)
-  (declare (optimize #+easy-audio-unsafe-code
-                     (safety 0) (speed 3))
-           (type (sa-sb 32) output channel1 channel2))
+  (declare (optimize (speed 3)))
   (loop for i below (length channel1)
         for j from 0 by 2 do
-       (setf (aref output j)
-             (aref channel1 i)
-             (aref output (1+ j))
-             (aref channel2 i)))
+        (setf (aref output j)
+              (aref channel1 i)
+              (aref output (1+ j))
+              (aref channel2 i)))
   output)
 
+(sera:-> mixchannels ((sa-sb 32) list)
+         (values (sa-sb 32) &optional))
 (defun mixchannels (out buffers)
-  "Maps a list of @c(buffers) (each one for each channel) into one buffer @c(out)
-writing sequentially the first sample of the first channel then the first sample of
-second channel and so on until final channel is reached. When process repeats for
-second sample of each channel until all data is written"
-  (declare (optimize (speed 3))
-           (type list buffers))
+  "Maps a list of @c(buffers) (each one for each channel) into one
+buffer @c(out) writing sequentially the first sample of the first
+channel then the first sample of second channel and so on until final
+channel is reached. When process repeats for second sample of each
+channel until all data is written."
+  (declare (optimize (speed 3)))
   (case (length buffers)
-    (2 (mixchannels-2 out
-                      (first buffers)
-                      (second buffers)))
+    (2 (mixchannels-2 out (first buffers) (second buffers)))
     (t (mixchannels-n out buffers))))
 
 (defmacro define-documented-accessor (structure slot docstring)
